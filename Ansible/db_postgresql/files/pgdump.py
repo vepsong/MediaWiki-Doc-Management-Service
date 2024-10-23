e3e8bde0a4d812cb5a92e2504e569e2a61e73d4e
Mitya <dyakirsanov@gmail.com>
2024-10-23 05:33:27 +0000

    change

--- Ansible/db_postgresql/files/pgdump.py
+++ Ansible/db_postgresql/files/pgdump.py
@@ -23,23 +23,36 @@ PRIVATE_KEY_PATH = Path(os.getenv("PRIVATE_KEY_PATH"))
 REMOTE_HOST = os.getenv("REMOTE_HOST")
 REMOTE_USER = os.getenv("REMOTE_USER")
 
-
-
-# под удаление
-SQL_DUMP_FILE_NAME = f'dump_{DATABASE_NAME}_{NOW}.sql'
-ARCHIVE_FILE_NAME = f'archive_sql_dump_{DATABASE_NAME}_and_mediawiki_folder_{NOW}.tar.gz'
-
 REMOTE_PATH = '/var/www/mediawiki'
 MEDIAWIKI_FOLDER_NAME = 'mediawiki'
 
-ARCHIVE_SQL_DUMP_FILE_NAME = f'dump_{DATABASE_NAME}_{NOW}.sql'
+ARCHIVE_SQL_DUMP_FILE_NAME = f'dump_sql_{DATABASE_NAME}_{NOW}.tar.gz'.replace('/', '_')
 ARCHIVE_MEDIAWIKI_REMOTE_FOLDER_NAME = f'backup_{REMOTE_HOST}_{REMOTE_PATH}_{NOW}.tar.gz'.replace('/', '_')
 
-BACKUP_PATTERN = re.compile(r"archive_sql_dump_(.+)_and_mediawiki_folder_(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})\.tar\.gz")
+BACKUP_SQL_PATTERN = re.compile(r"archive_sql_dump_(.+)_and_mediawiki_folder_(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})\.tar\.gz")
+BACKUP_SQL_PATTERN = re.compile(r"archive_sql_dump_(.+)_and_mediawiki_folder_(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})\.tar\.gz")
 KEEP_LAST_N_BACKUPS = 10  # Количество бэкапов, которые нужно ост
 
 
-def get_remote_folder():
+def make_archive(dest_path, source_path):
+    """Архивирование .tar.gz."""
+
+    try:
+        # Команда для создания архива
+        tar_command = ['tar', '-czvf', dest_path, '-C', source_path, '.']
+        # Выполнение команды архивации
+        tar_result = subprocess.run(tar_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        print(f"Архивация успешно завершена: {NOW}")
+        print(tar_result.stdout.decode('utf-8'))
+
+    except subprocess.CalledProcessError as e:
+        print("Ошибка при создании архива:")
+        print(e.stderr.decode('utf-8'))
+
+
+def get_and_archive_remote_folder():
+    """rsync удаленной директории на локальную ВМ и архивация."""
     try:
         command = [
             'rsync',
@@ -57,65 +70,37 @@ def get_remote_folder():
         print("Результат синхронизации:")
         print(result.stdout.decode('utf-8'))
 
-        # Создание архива после синхронизации
-        archive_path = f'{BACKUPS_PATH}/{ARCHIVE_MEDIAWIKI_REMOTE_FOLDER_NAME}'
-        mediawiki_folder_path = f'{BACKUPS_PATH}/{MEDIAWIKI_FOLDER_NAME}'
-
-        # Команда для создания архива
-        tar_command = ['tar', '-czvf', archive_path, '-C', mediawiki_folder_path, '.']
-        # Выполнение команды архивации
-        tar_result = subprocess.run(tar_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        print("Архивация завершена:")
-        print(tar_result.stdout.decode('utf-8'))
-
     except subprocess.CalledProcessError as e:
         print("Ошибка при синхронизации:")
         print(e.stderr.decode('utf-8'))
 
-
-
-
-def create_backup_combine_archive(now):
-    try:
-        print("create_backup_file started")
-        combine_archive_filename = f'{BACKUPS_PATH}/{ARCHIVE_FILE_NAME}'
+    # Создание архива после синхронизации
+    dest_path = f'{BACKUPS_PATH}/{ARCHIVE_MEDIAWIKI_REMOTE_FOLDER_NAME}'
+    source_path = f'{BACKUPS_PATH}/{MEDIAWIKI_FOLDER_NAME}'
         
+    make_archive(dest_path, source_path)
 
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            sql_dump_filepath = f'{Path(tmpdirname)}/{SQL_DUMP_FILE_NAME}'
 
-            create_dump_postgres(sql_dump_filepath)
-
-            archive_tar_command = (
-            f'tar -czf {combine_archive_filename} '
-            f'-C {sql_dump_filepath} -C {BACKUPS_PATH}/{MEDIAWIKI_FOLDER_NAME}'
-            )
-
-            result = subprocess.run(archive_tar_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            if result != 0:
-                raise Exception(f"tar failed with code {result}, command = {archive_tar_command}")
-
-    except Exception as e:
-        print(f"Error in create_backup_file: {e}")
-        sys.exit(1)
 
 
 # Создание sql-dump'a БД"
 def create_dump_postgres(sql_dump_filepath):
     """Создание sql-dump'a БД"""
     try:
-        pg_dump_command = (
-            f'PGPASSWORD={DATABASE_PASSWORD} '
-            f'pg_dump -U {DATABASE_USER} {DATABASE_NAME} -p {DATABASE_PORT} '
-            f'-h localhost > {sql_dump_filepath}'
-        )
-
-        result = subprocess.run(pg_dump_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        dest_path = f'{BACKUPS_PATH}/{ARCHIVE_SQL_DUMP_FILE_NAME}'
+        command = [
+            'pg_dump',
+            f"{DATABASE_NAME}",
+            '| gzip |',
+            'tar -cv',
+            f"{dest_path}"
+        ]
+                
+        # result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         if result != 0:
-            raise Exception(f"pg_dump failed with code {result}, command = {pg_dump_command}")
+            raise Exception(f"pg_dump failed with code {result.returncode}, command = {command}")
     except Exception as e:
         print(f"Error in create_dump_postgres: {e}")
         sys.exit(10)
@@ -156,11 +141,12 @@ if __name__ == "__main__":
     try:
         print(f"Backup script started: {NOW}")
 
-        get_remote_folder()
-        create_backup_combine_archive(NOW)
-        rotate_backups(NOW)
+        get_and_archive_remote_folder()
+        create_dump_postgres()
+        rotate_backups()
 
-        print(f"Backup prod data script was successfully ended. timestamp: {NOW}")
+        print(f"Backup script was successfully ended: {NOW}")
+
     except Exception as e:
         print(f"Error occurred: {e}")
 
